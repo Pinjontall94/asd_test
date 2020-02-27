c142e3f2fdcb4f4e7bc469c5a0a85653d640f551
Sam Johnson <sambjohnson94@gmail.com>
2020-02-26 20:40:43 -0800

    pulled updated 16S extractor from barbera

--- 4_16S_extractor.sh
+++ 4_16S_extractor.sh
@@ -7,29 +7,28 @@
 # OUTPUT: 16S/,*.fastq
 
 FPRIMER="GTGCCAGC(A|C)GCCGCGGTAA"
-RUNTABLE=SraRunTable.csv
+RUNTABLE=SraRunTable.txt
 
-if [ -f *.fastq ] && [ ! -d 16S ]; then
-	mkdir 16S 
-	if [ -f SraRunTable.csv ]; then
-		awk -F "," '/16S/ { print $1 }' $RUNTABLE > 16S_list.txt.temp
-#		awk -F "," '/16S/ { print $1 }' $RUNTABLE > 16S_list.txt.temp
-		while IFS= ; do read line
-			echo "$line"
-			mv $line 16S
-		done < 16S_list.txt.temp
-		rm 16S_list.txt.temp
-	else	
-		echo "No SraRunTable.csv found! Try grep -B 1 -E \"<FORWARD PRIMER>\" on fastqs"
-		echo "\nExample output:"
-		grep -EB 1 $FPRIMER
-		# Put grep tool for generating 16S list here 
-		# 	(Note: Add both "FPRIMER" __and__ "^FPRIMER" versions, and separate
-		# 	their results in different directories)
+mkdir 16S_temp filter
+if [ -f $RUNTABLE ]; then
+	awk -F "," '/16S/ { print $1 }' $RUNTABLE > 16S_list.txt.temp
+	while read line; do
+		echo "$line.fastq"
+		eval mv $line*.fastq 16S_temp
+	done < 16S_list.txt.temp
+	rm 16S_list.txt.temp
+	mv *.fastq filter 
+	mv 16S_temp/* . && rmdir 16S_temp
+else	
+	echo "No SraRunTable.csv found! Try grep -B 1 -E \"<FORWARD PRIMER>\" on fastqs"
+	echo "\nExample output:"
+	grep -EB 1 $FPRIMER
+	# Put grep tool for generating 16S list here 
+	# 	(Note: Add both "FPRIMER" __and__ "^FPRIMER" versions, and separate
+	# 	their results in different directories)
 #	        head -n 2 *.fastq \
 #	        | grep -EB 1 $FPRIMER \
 #	        | grep -o "SRR[0-9]\+" \
 #	        | sed 's/$/.fastq/'
-        	echo "Error! No fasta or fastq files found in current directory"
-	fi
+        echo "Error: No fastq files found in current directory"
 fi
